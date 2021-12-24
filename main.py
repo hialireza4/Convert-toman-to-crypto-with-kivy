b5559cb39ccd647ec67b1286ff91f2fb3571e5ea
Alireza Ahmadi <hialireza4@gmail.com>
2021-12-24 11:12:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ ScreenManager:
             id:toman
             hint_text: "Enter Price"
             mode: "rectangle"
-	    input_type : "number"
+	    input_filter: 'int'
             text: ""
             font_size:16
             pos_hint: {"center_x": .65, "center_y": .80}
